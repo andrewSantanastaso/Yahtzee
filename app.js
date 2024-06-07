bd94ff767830ff8674557aa13ac7dcbe91409c40
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-06-07 09:37:41 -0400

    created some variables and constants

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const MAXTURN = 26
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let player1Score
+let player2Score
+let turnNumber
+let lockedDice = []
+let player1Turn = false
+let player2Turn = false
 
 /*------------------------ Cached Element References ------------------------*/
 
